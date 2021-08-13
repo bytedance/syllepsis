f1348982fd3ad403a78e31bb2685360850d85350
lastnigtic <lastnigtic@gmail.com>
2021-08-13 11:42:22 +0800

    test(test): add html event test

--- test/case/adapter/other.test.ts
+++ test/case/adapter/other.test.ts
@@ -107,6 +107,30 @@ describe('MountEvent test', () => {
   });
 });
 
+describe('Extra test', () => {
+  test('not invoke event on html', async () => {
+    const val = await page.evaluate(() => {
+      return new Promise(resolve => {
+        let v = 0;
+        Object.defineProperty(window, 'variable', {
+          get() {
+            return v;
+          },
+          set(val) {
+            v = val;
+            resolve(v);
+            return v;
+          },
+        });
+        editor.setHTML('<img src="gg" onerror="window.variable=2" />');
+        setTimeout(() => resolve(window.variable), 4500);
+      });
+    });
+
+    expect(val).toEqual(0);
+  });
+});
+
 describe('final test', () => {
   it('it can uninstall', async () => {
     const res = await page.evaluate(() => {
