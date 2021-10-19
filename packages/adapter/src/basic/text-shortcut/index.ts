1a4a175c88b9f98db933a075b28fce146876f04a
lastnigtic <lastnigtic@gmail.com>
2021-10-19 11:03:06 +0800

    refactor(Controller): remove `Controller` supports of `textMatcher`
    BREAKING CHANGE: `Controller` no longer supports `textMatcher`

--- packages/adapter/src/basic/text-shortcut/index.ts
+++ packages/adapter/src/basic/text-shortcut/index.ts
@@ -1,5 +1,4 @@
 export * from './block-leaf-shortcut';
-export * from './controller-shortcut';
 export * from './inline-leaf-shortcut';
 export * from './rule-builder';
 export * from './shortcut-plugin';
