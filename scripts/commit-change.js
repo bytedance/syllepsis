0bc0d179cfc97d9b915162a7c8d48fc39fe9bb3d
lastnigtic <lastnigtic@gmail.com>
2022-02-10 14:31:04 +0800

    chore: Use `spawnSync` instead

--- scripts/commit-change.js
+++ scripts/commit-change.js
@@ -1,21 +1,11 @@
-const spawn = require('child_process').spawn
+const spawn = require('child_process').spawnSync;
 
-const sp = spawn('git', [
-  'diff',
-  '--name-only'
-])
+const sp = spawn('git', ['diff', '--name-only']);
 
-sp.stdout.on('data', (data) => {
+sp.stdout.on('data', data => {
   if (data.toString().includes('CHANGELOG.md')) {
-    spawn('git', [
-      'add',
-      'CHANGELOG.md'
-    ])
+    spawn('git', ['add', 'CHANGELOG.md']);
 
-    spawn('git', [
-      'commit',
-      '-m',
-      'docs: update CHANGELOG'
-    ])
+    spawn('git', ['commit', '-m', 'docs: update CHANGELOG']);
   }
-})
+});
