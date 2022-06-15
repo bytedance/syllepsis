52eaee9dd28dc4c7ce827b57fea65d722d1d9880
lastnigtic <lastnigtic@gmail.com>
2022-06-15 17:16:10 +0800

    chore: Read commit type by lower case

--- changelog/config.js
+++ changelog/config.js
@@ -4,29 +4,30 @@ const { join } = require('path');
 const logType = ['feat', 'fix', 'perf', 'revert', 'refactor'];
 
 const showType = {
-	feat: '🎉 Features',
-	fix: '🐞 Bug Fixers',
-	perf: '🌟 Performance Improve',
-	revert: '🤚 Reverts',
-	refactor: '🔨 Refactor',
+  feat: '🎉 Features',
+  fix: '🐞 Bug Fixers',
+  perf: '🌟 Performance Improve',
+  revert: '🤚 Reverts',
+  refactor: '🔨 Refactor',
 };
 
 module.exports = {
-	writerOpts: {
-		transform: (commit) => {
-			if (!logType.includes(commit.type)) {
-				return false;
-			}
-			commit.type = showType[commit.type];
-			return commit;
-		},
-		generateOn: (commit) => {
-			if (!/-/.test(commit.version)) {
-				return commit.version;
-			}
-			return false;
-		},
-		mainTemplate: readFileSync(join(__dirname, 'template.hbs'), 'utf-8'),
-		commitPartial: readFileSync(join(__dirname, 'commit.hbs'), 'utf-8'),
-	},
+  writerOpts: {
+    transform: commit => {
+      const commitType = commit.type.toLowerCase();
+      if (!logType.includes(commitType)) {
+        return false;
+      }
+      commit.type = showType[commitType];
+      return commit;
+    },
+    generateOn: commit => {
+      if (!/-/.test(commit.version)) {
+        return commit.version;
+      }
+      return false;
+    },
+    mainTemplate: readFileSync(join(__dirname, 'template.hbs'), 'utf-8'),
+    commitPartial: readFileSync(join(__dirname, 'commit.hbs'), 'utf-8'),
+  },
 };
